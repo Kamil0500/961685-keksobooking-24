be69890fe28af99430c49afcec209ea124e77d57
Kamil0500 <kamskiy1985@mail.ru>
2021-09-25 23:37:04 +0300

    исправление синтаксических ошибок

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const getRandomNumber = function (min, max, decimalPlaces = 0) {
   if (min > max || min < 0 || max <= 0) {
-    console.log("Введён неверный диапазон чисел");
+
     return false;
   }
   return (Math.random() * (max - min) + min).toFixed(decimalPlaces);
