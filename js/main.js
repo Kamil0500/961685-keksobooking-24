7a7abdac7c294953bd46b2e8da7d27c7ada4dc3e
Kamil0500 <kamskiy1985@mail.ru>
2021-10-05 19:44:06 +0300

    больше деталей

--- js/main.js
+++ js/main.js
@@ -7,3 +7,82 @@ const getRandomNumber = function (min, max, decimalPlaces = 0) {
 };
 
 getRandomNumber();
+
+const avatar = [
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png',
+  'img/avatars/user09.png',
+  'img/avatars/user10.png'
+];
+
+const title = [
+  'Квартира',
+  'Дом',
+  'Комната'
+];
+
+const address = ['location.lat', 'location.lng'];
+const price = [5000, 10000, 50000];
+
+const type = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+const rooms = [1, 2, 3, 4, 5];
+const guests = [1, 2, 3, 4, 5];
+const checkin = ['12:00', '13:00', '14:00'];
+const checkout = ['12:00', '13:00', '14:00'];
+
+const features = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const description = ['Просторная, уютная, со всеми удобствами'];
+
+const photos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+const lat = [35.65000, 35.70000];
+const lng = [139.70000, 139.80000];
+
+
+const countObject = 10;
+
+const getRandomElement = (element) => {
+ return element [(Math.floor(Math.random() * (element.length -1)))]
+};
+
+let getLocation = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const newArray = () => {
+  return {
+    author: getRandomElement(avatar),
+    offer: getRandomElement(title) + ' ' + getRandomElement(address) + ' ' + getRandomElement(price) + ' ' + getRandomElement(type) + ' ' + getRandomElement(rooms) + ' ' + getRandomElement(guests) + ' ' + getRandomElement(checkin) + ' ' + getRandomElement(checkout) + ' ' +  getRandomElement(features) + ' ' + getRandomElement(description),
+    photo: getRandomElement(photos),
+    location: getRandomElement(lat) + ' ' + getRandomElement(lng)
+  };
+};
+
+const countArray = Array.from({length: countObject}, newArray);
+
+console.log(countArray);
