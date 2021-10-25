90ff05fa20d22602bcec1b18802faf451a5f7f11
Kamil0500 <kamskiy1985@mail.ru>
2021-10-25 10:07:01 +0300

    валидация формы

--- js/module/form.js
+++ js/module/form.js
@@ -1,40 +1,47 @@
-const form = document.querySelector('.ad-form');
-const rooms = form.querySelector('#room_number');
-const guests = form.querySelector('#capacity');
-const price = form.querySelector('#price');
-const timeIn = form.querySelector('#timein');
-const timeOut = form.querySelector('#timeout');
+const formElement = document.querySelector('.ad-form');
+const roomsInputElement = document.querySelector('#room_number');
+const guestInputElement = document.querySelector('#capacity');
+const priceInputElement = document.querySelector('#price');
+const typeInputElement = document.querySelector('#type');
+const timeInInput = document.querySelector('#timein');
+const timeOutInput = document.querySelector('#timeout');
 
-const ofGuestsAndRooms = () => {
-  if (rooms.value !== '100' && guests.value === '0') {
-    guests.setCustomValidity('Мало гостей');
-  } else if (rooms.value < guests.value) {
-    guests.setCustomValidity('Много гостей');
-  } else if (rooms.value === '100' && guests.value !== '0') {
-    guests.setCustomValidity('Не для гостей');
-  } else {
-    guests.setCustomValidity('');
+const minPriceRooms = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+
+const onChangeTypeElement = function () {
+  priceInputElement.placeholder = minPriceRooms[typeInputElement.value];
+  priceInputElement.min = minPriceRooms[typeInputElement.value];
+};
+onChangeTypeElement();
+
+const onTimeChange = function (evt) {
+  switch (evt.target) {
+    case timeInInput:
+      timeOutInput.value = timeInInput.value;
+      break;
+      case timeOutInput:
+        timeInInput.value = timeOutInput.value;
+        break;
   }
 };
 
-rooms.addEventListener('change', () => {
-  ofGuestsAndRooms();
-});
-
-guests.addEventListener('change', () => {
-  ofGuestsAndRooms();
-});
-
-form.addEventListener('click', () => {
-  ofGuestsAndRooms();
-});
-
-const onTimeIn = function (evt) {
-  timeIn.value = evt.target.value;
-};
-const onTimeOut = function (evt) {
-  timeOut.value = evt.target.value;
+const onRoomsChange = function (evt) {
+  switch (evt.target) {
+    case roomsInputElement:
+      guestInputElement.value = roomsInputElement.value;
+      break;
+      case guestInputElement:
+        roomsInputElement.value = guestInputElement.value;
+        break;
+  }
 };
 
-form.addEventListener('change', onTimeIn);
-form.addEventListener('change', onTimeOut);
+formElement.addEventListener('change', onRoomsChange);
+formElement.addEventListener('change', onTimeChange);
+typeInputElement.addEventListener('change', onChangeTypeElement);
