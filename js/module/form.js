1797abd63f8427c7ecf910c374867d02302718ef
Kamil0500 <kamskiy1985@mail.ru>
2021-10-25 10:10:22 +0300

    валидация формы

--- js/module/form.js
+++ js/module/form.js
@@ -1,10 +1,10 @@
-const formElement = document.querySelector('.ad-form');
-const roomsInputElement = document.querySelector('#room_number');
-const guestInputElement = document.querySelector('#capacity');
-const priceInputElement = document.querySelector('#price');
-const typeInputElement = document.querySelector('#type');
-const timeInInput = document.querySelector('#timein');
-const timeOutInput = document.querySelector('#timeout');
+const formElement = document.querySelector(".ad-form");
+const roomsInputElement = document.querySelector("#room_number");
+const guestInputElement = document.querySelector("#capacity");
+const priceInputElement = document.querySelector("#price");
+const typeInputElement = document.querySelector("#type");
+const timeInInput = document.querySelector("#timein");
+const timeOutInput = document.querySelector("#timeout");
 
 const minPriceRooms = {
   bungalow: 0,
@@ -25,9 +25,9 @@ const onTimeChange = function (evt) {
     case timeInInput:
       timeOutInput.value = timeInInput.value;
       break;
-      case timeOutInput:
-        timeInInput.value = timeOutInput.value;
-        break;
+    case timeOutInput:
+      timeInInput.value = timeOutInput.value;
+      break;
   }
 };
 
@@ -36,12 +36,12 @@ const onRoomsChange = function (evt) {
     case roomsInputElement:
       guestInputElement.value = roomsInputElement.value;
       break;
-      case guestInputElement:
-        roomsInputElement.value = guestInputElement.value;
-        break;
+    case guestInputElement:
+      roomsInputElement.value = guestInputElement.value;
+      break;
   }
 };
 
-formElement.addEventListener('change', onRoomsChange);
-formElement.addEventListener('change', onTimeChange);
-typeInputElement.addEventListener('change', onChangeTypeElement);
+formElement.addEventListener("change", onRoomsChange);
+formElement.addEventListener("change", onTimeChange);
+typeInputElement.addEventListener("change", onChangeTypeElement);
