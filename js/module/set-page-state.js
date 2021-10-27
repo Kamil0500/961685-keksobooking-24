77c448f5fcb0a44f7cbfda56121cee69b48a79f0
Kamil0500 <kamskiy1985@mail.ru>
2021-10-27 23:21:09 +0300

    состояние активной и неактивной формы

--- js/module/set-page-state.js
+++ js/module/set-page-state.js
@@ -1,7 +1,7 @@
 const forms = document.querySelectorAll('form');
 
 const setElementDisabled = (form) => {
-  form.querySelectorAll('form select, input, button, textarea').forEach((item) => item.disabled = true)
+  form.querySelectorAll('form select, input, button, textarea').forEach((item) => item.disabled = true);
 };
 
 const setElementEnabled = (form) => {
@@ -16,6 +16,6 @@ export const setDisabledForm = () => {
 };
 
 export const setEnabledForm = (form) => {
-    form.classList.remove('ad-form--disabled');
-    setElementEnabled(form);
+  form.classList.remove('ad-form--disabled');
+  setElementEnabled(form);
 };
