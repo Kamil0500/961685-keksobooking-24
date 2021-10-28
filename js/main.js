11efc55562aaedc0fe9cb04d48177149f6166797
Kamil0500 <kamskiy1985@mail.ru>
2021-10-28 07:13:08 +0300

    исправление функций состояния формы

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 import {cardListFragment} from './module/generation-elements.js';
 import {validateForm} from './module/form.js';
-import {setEnabledForm, setDisabledForm} from './module/set-page-state.js';
+import {setDisabledFormState, setEnabledFormState} from './module/set-page-state.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
+const addFormElement = document.querySelector('.ad-form');
+const filterFormElement = document.querySelector('.map__filters');
 mapCanvas.appendChild(cardListFragment);
 
 validateForm();
-setEnabledForm();
-setDisabledForm();
+
+setDisabledFormState();
+setEnabledFormState(addFormElement);
+setEnabledFormState(filterFormElement);
+
--- js/module/set-page-state.js
+++ js/module/set-page-state.js
@@ -1,21 +1,21 @@
 const forms = document.querySelectorAll('form');
 
-const setElementDisabled = (form) => {
+const setElementDisabledState = (form) => {
   form.querySelectorAll('form select, input, button, textarea').forEach((item) => item.disabled = true);
 };
 
-const setElementEnabled = (form) => {
+const setElementEnabledState = (form) => {
   form.querySelectorAll('form select, input, button, textarea').forEach((item) => item.disabled = false);
 };
 
-export const setDisabledForm = () => {
+export const setDisabledFormState = () => {
   forms.forEach((form) => {
-    form.classList.add('ad-form--disabled');
-    setElementDisabled(form);
+    form.classList.add('disabled');
+    setElementDisabledState(form);
   });
 };
 
-export const setEnabledForm = (form) => {
-  form.classList.remove('ad-form--disabled');
-  setElementEnabled(form);
+export const setEnabledFormState = (form) => {
+  form.classList.remove('disabled');
+  setElementEnabledState(form);
 };
