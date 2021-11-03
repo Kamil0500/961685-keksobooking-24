7b1b42aa686889ec19cdd6f763869e4450dcc6d2
Kamil0500 <kamskiy1985@mail.ru>
2021-11-03 23:08:10 +0300

    карта

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 import {validateForm} from './module/form.js';
 import {setDisabledFormState, setEnabledFormState} from './module/set-page-state.js';
-import './module/map.js';
+import {initMap} from './module/map.js';
 
-const addFormElement = document.querySelector('.ad-form');
 const filterFormElement = document.querySelector('.map__filters');
 
 validateForm();
 
 setDisabledFormState();
-setEnabledFormState(addFormElement);
+initMap();
 setEnabledFormState(filterFormElement);
 
